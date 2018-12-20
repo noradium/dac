a339b240731c3af3bf2c2b23d15e2149c12ded8b
noradium <noradium888@gmail.com>
2018-12-20 17:06:04 +0900

    url が変わって壊れていたのを修正

--- src/scripts/modules/video_info.js
+++ src/scripts/modules/video_info.js
@@ -20,7 +20,7 @@ export default class VideoInfo {
   constructor() {
     // この辺りのデータは`#js-initial-watch-data`から取れると良かったのですが、これだと説明文などから動画を移動したときに正しい値にならない。
     const channelLink = document.querySelector('.ChannelInfo-pageLink');
-    this._channelId = channelLink ? channelLink.getAttribute('href').match(/^http:\/\/ch\.nicovideo\.jp\/(ch[0-9]+)/)[1] : null;
+    this._channelId = channelLink ? channelLink.getAttribute('href').match(/^https?:\/\/ch\.nicovideo\.jp\/(ch[0-9]+)/)[1] : null;
     this._title = document.querySelector('.VideoTitle').innerText;
     this._duration = document.querySelector('.PlayerPlayTime-duration').innerText.split(':').reduce((prev, current, index, source) => {
       return prev + current * Math.pow(60, source.length - 1 - index);
