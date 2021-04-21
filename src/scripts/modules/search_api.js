13cf5dfb513071f9787e7342fdb27c42c9836d95
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2021-04-21 18:05:41 +0900

    add: a

--- src/scripts/modules/search_api.js
+++ src/scripts/modules/search_api.js
@@ -4,12 +4,11 @@
  */
 export default class SearchAPI {
   static fetch(word, limit = 10) {
-    return window.fetch(`https://api.search.nicovideo.jp/api/v2/video/contents/search?q=${encodeURIComponent(word)}&targets=title&_sort=-commentCounter&fields=title,threadId,channelId,lengthSeconds,thumbnailUrl,commentCounter&_limit=${limit}&_context=danime-another-comment`)
+    return window.fetch(`https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${encodeURIComponent(word)}&targets=title&_sort=-commentCounter&fields=title,contentId,lengthSeconds,thumbnailUrl,commentCounter&_limit=${limit}&_context=danime-another-comment`)
       .catch((error) => {
+        console.error(error)
         return Promise.reject('search_error');
       })
-      .then((response) => {
-        return response.json();
-      });
+      .then((response) => response.json());
   }
 }
